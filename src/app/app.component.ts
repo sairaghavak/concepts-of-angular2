b5e05df328be0d559b5657c811affb69791463e1
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-03-12 22:15:30 +0530

    Updated the temporary static message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Welcome to the concepts-of-angular2! This page is under development, angular2 concepts will be demonstrated slowly..';
+  title = 'Welcome to the concepts-of-angular2!';
 }
