76fde6fb52833b0afea202924fb1b977f06f593e
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-03-21 22:15:15 +0530

    ng lint errors fixed

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,6 +4,6 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'app-home',
     template:
-    `<b>Congrats you are in home component, that is lazy loaded from Home module</b>`
+    `<b>Congrats you are in home component, that is eagerly loaded from the app module</b>`
 })
 export class HomeComponent { }
