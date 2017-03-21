e37ac1c418feb379aece62b508591112e79131dd
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-03-21 22:15:40 +0530

    ng lint errors fixed

--- src/app/page-not-found.component.ts
+++ src/app/page-not-found.component.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector : 'page-not-found',
-    template: 
+    selector : 'app-page-not-found',
+    template:
     `
         <p><b>Page Not Found</b></p>
     `
