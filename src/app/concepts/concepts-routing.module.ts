c2e1bb761b747089c8534cfac8a8072176e3eb7f
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-01 11:03:57 +0530

    added all the routes in concepts routing module

--- src/app/concepts/concepts-routing.module.ts
+++ src/app/concepts/concepts-routing.module.ts
@@ -1,13 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-import { ModulesComponent } from './modules.component';
-import { ComponentsComponent } from './components.component';
+import { ModulesComponent } from './modules/modules.component';
+import { ComponentsComponent } from './components/components.component';
+import { TemplatesComponent } from './templates/templates.component';
+import { MetadataComponent } from './metadata/metadata.component';
+import { DataBindingComponent } from './data-binding/data-binding.component';
+import { DirectivesComponent } from './directives/directives.component';
+import { ServicesComponent } from './services/services.component';
+import { DependencyInjectionComponent } from './dependency-injection/dependency-injection.component';
+
 import { ConceptNotFoundComponent } from './concept-not-found.component';
 
 const conceptsRoutes: Routes = [
     { path: 'modules', component: ModulesComponent },
     { path: 'components', component: ComponentsComponent },
+    { path: 'templates', component: TemplatesComponent },
+    { path: 'metadata', component: MetadataComponent },
+    { path: 'data-binding', component: DataBindingComponent },
+    { path: 'directives', component: DirectivesComponent },
+    { path: 'services', component: ServicesComponent },
+    { path: 'dependency-injection', component: DependencyInjectionComponent },
     { path: '**', component: ConceptNotFoundComponent }
 ];
 
@@ -16,4 +29,4 @@ const conceptsRoutes: Routes = [
     exports: [RouterModule]
 })
 export class ConceptsRoutingModule { }
-export const conceptsRoutingComponents = [ModulesComponent, ComponentsComponent, ConceptNotFoundComponent];
+export const CONCEPTS_ROUTING_COMPONENTS = [ModulesComponent, ComponentsComponent, TemplatesComponent, MetadataComponent, DataBindingComponent, DirectivesComponent, ServicesComponent, DependencyInjectionComponent, ConceptNotFoundComponent];
