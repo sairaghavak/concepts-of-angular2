c2ed0fce2216c86fcf96d08cae9246bbcc7b985b
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-03 15:47:21 +0530

    Updated footer component by adding copyright

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  private _year: number;
+
   constructor() { }
 
   ngOnInit() {
+    this._year = new Date().getFullYear();
   }
 
 }
