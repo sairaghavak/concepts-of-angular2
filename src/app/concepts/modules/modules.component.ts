0bbaa46f75156af5bf01f9c115674f19f205e08a
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-03 15:46:35 +0530

    default code block added by importing file from md file

--- src/app/concepts/modules/modules.component.ts
+++ src/app/concepts/modules/modules.component.ts
@@ -1,22 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
-import { Constants } from '../constants';
+import { Constants } from '../../shared-services/constants';
+import { FileService } from '../../shared-services/FileService';
+import { ISubscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-modules',
   templateUrl: './modules.component.html',
   styleUrls: ['./modules.component.css']
 })
-export class ModulesComponent implements OnInit {
+export class ModulesComponent implements OnInit, OnDestroy {
+  private subscriber: ISubscription;
+  private codeSnippet: string;
+  private moduleErrorMessage: string;
+  private readonly modules_snippet_file: string = Constants.MODULE_FILE_PATH;
+  constructor(private _parentRouter: Router, private _activateRoute: ActivatedRoute, private _fileService: FileService) { }
 
-  readonly gistModuleId: string;
+  ngOnInit() {
+    this.subscriber = this._fileService.getFile(this.modules_snippet_file).subscribe(
+      response => {
+        this.codeSnippet = response;
+      },
+      error => {
+        this.moduleErrorMessage = error;
+      },
+      () => {
+        console.log("Successfully loaded file.");
+      }
 
-  constructor(private _parentRouter: Router, private _activateRoute: ActivatedRoute) {
-    this.gistModuleId = Constants.getModuleGistId();
+
+    );
   }
 
-  ngOnInit() { }
-
+  ngOnDestroy() {
+    this.subscriber.unsubscribe();
+    console.log("unsubscribed..");
+  }
   goToMetaData() {
     this._parentRouter.navigate(['../metadata'], { relativeTo: this._activateRoute });
   }
