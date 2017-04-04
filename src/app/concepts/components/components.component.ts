9bb8b9ac196f1b0703bbdf4fd8454b5234281334
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-04 16:11:08 +0530

    Components concepts is added.

--- src/app/concepts/components/components.component.ts
+++ src/app/concepts/components/components.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from '../../shared-services/constants';
+import { FileService } from '../../shared-services/FileService';
 
 @Component({
   selector: 'app-components',
@@ -7,9 +9,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ComponentsComponent implements OnInit {
 
-  constructor() { }
+  codeSnippets: string[] = [];
+  isLoading: boolean[] = [];
+
+  private readonly components_code_snippets: string[];
+  private componentErrorMessage: string;
+
+  constructor(private _fileService: FileService) {
+    this.components_code_snippets = [Constants.COMPONENTS_HTML_FILE_PATH, Constants.COMPONENTS_FILE_PATH];
+  }
 
   ngOnInit() {
+
+    for (let index in this.components_code_snippets) {
+      this.subscribeAndGetFile(index);
+    }
+  }
+
+
+  subscribeAndGetFile(index: string) {
+    this._fileService.getFile(this.components_code_snippets[index]).subscribe(
+      response => {
+        this.codeSnippets.push(response);
+      },
+      error => {
+        this.componentErrorMessage = error;
+      },
+      () => {
+        this.isLoading.push(true);
+      }
+    );
   }
 
 }
