771d2a0a9f1bc161d053c116a905952f6c644be0
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-04 16:11:54 +0530

    Removed the unsubscribe() as it is not required for HTTP requests and is taken care by Angular2.

--- src/app/concepts/modules/modules.component.ts
+++ src/app/concepts/modules/modules.component.ts
@@ -2,23 +2,23 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router, ActivatedRoute } from '@angular/router';
 import { Constants } from '../../shared-services/constants';
 import { FileService } from '../../shared-services/FileService';
-import { ISubscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-modules',
   templateUrl: './modules.component.html',
   styleUrls: ['./modules.component.css']
 })
-export class ModulesComponent implements OnInit, OnDestroy {
-  isLoading:boolean;
-  private subscriber: ISubscription;
+export class ModulesComponent implements OnInit {
+  isLoading: boolean;
   codeSnippet: string;
+
   private moduleErrorMessage: string;
   private readonly modules_snippet_file: string = Constants.MODULE_FILE_PATH;
+
   constructor(private _parentRouter: Router, private _activateRoute: ActivatedRoute, private _fileService: FileService) { }
 
   ngOnInit() {
-    this.subscriber = this._fileService.getFile(this.modules_snippet_file).subscribe(
+    this._fileService.getFile(this.modules_snippet_file).subscribe(
       response => {
         this.codeSnippet = response;
       },
@@ -29,15 +29,9 @@ export class ModulesComponent implements OnInit, OnDestroy {
         this.isLoading = true;
         console.log("Successfully loaded file.");
       }
-
-
     );
   }
 
-  ngOnDestroy() {
-    this.subscriber.unsubscribe();
-    console.log("unsubscribed..");
-  }
   goToMetaData() {
     this._parentRouter.navigate(['../metadata'], { relativeTo: this._activateRoute });
   }
