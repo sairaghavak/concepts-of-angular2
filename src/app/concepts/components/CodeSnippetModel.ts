810a5ae45d8878bd67cf58774212d497b6cdd86e
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-04 17:55:45 +0530

    Code snippet model for displaying code snippets for this component in UI.

---
+++ src/app/concepts/components/CodeSnippetModel.ts
@@ -0,0 +1,8 @@
+/**
+ *  A model that is shown in the template as a code snippet.
+ */
+export class CodeSnippetModel {
+    isLoading: boolean;
+    codeSnippetFilePath: string;
+    codeSnippet: string;
+}
