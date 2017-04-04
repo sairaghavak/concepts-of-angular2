26e7cc60007067e1ca9a8ff733f58b7f457dbf6e
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-04 23:15:25 +0530

    removed unused imports and variables

--- src/app/concepts/modules/modules.component.ts
+++ src/app/concepts/modules/modules.component.ts
@@ -1,5 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Component, OnInit} from '@angular/core';
 import { Constants } from '../../shared-services/constants';
 import { FileService } from '../../shared-services/FileService';
 
@@ -15,7 +14,7 @@ export class ModulesComponent implements OnInit {
   private moduleErrorMessage: string;
   private readonly modules_snippet_file: string = Constants.MODULE_FILE_PATH;
 
-  constructor(private _parentRouter: Router, private _activateRoute: ActivatedRoute, private _fileService: FileService) { }
+  constructor(private _fileService: FileService) { }
 
   ngOnInit() {
     this._fileService.getFile(this.modules_snippet_file).subscribe(
@@ -27,7 +26,6 @@ export class ModulesComponent implements OnInit {
       },
       () => {
         this.isLoading = true;
-        console.log("Successfully loaded file.");
       }
     );
   }
