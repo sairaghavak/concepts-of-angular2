b330e69f00bab689973eed6ed23452b958ef2f6d
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-04 16:13:24 +0530

    Additional constants added for Components concept

--- src/app/shared-services/constants.ts
+++ src/app/shared-services/constants.ts
@@ -1,12 +1,21 @@
 export class Constants {
 
     public static readonly CODE_SNIPPETS_ROOT_DIR: string = "assets/code-snippets/";
+
     public static readonly MODULE_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "modules.md";
+
     public static readonly COMPONENTS_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "components.md";
+    public static readonly COMPONENTS_HTML_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "components.html";
+
     public static readonly TEMPLATES_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "templates.md";
+
     public static readonly METADATA_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "metadata.md";
+
     public static readonly DATA_BINDING_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "data-binding.md";
+
     public static readonly DIRECTIVES_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "directives.md";
+
     public static readonly SERVICES_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "services.md";
+
     public static readonly DEPENDENCY_INJECTION_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "dependency-injection.md";
 }
