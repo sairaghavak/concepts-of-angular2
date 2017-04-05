8e24c41a3f5f0f3ddef7b320bb0586daf8049bfc
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-05 17:09:32 +0530

    Added to display inline code snippets in MetaData section.

---
+++ src/app/concepts/metadata/SmallCodeSnippet.ts
@@ -0,0 +1,7 @@
+/**
+ *  Serves the purpose of inline code snippets with their descriptions in metadata section
+ */
+export interface SmallCodeSnippet {
+    inlineCode: string;
+    codeDescription: string;
+}
