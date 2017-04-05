ea42400c1bc052ea1315e8615cafb505385853cb
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-05 17:10:10 +0530

    My understanding and thoughts on MetaData concept is added.

--- src/app/concepts/metadata/metadata.component.ts
+++ src/app/concepts/metadata/metadata.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Constants } from '../../shared-services/constants';
+import { FileService } from '../../shared-services/FileService';
+import { SmallCodeSnippet } from './SmallCodeSnippet'
 
 @Component({
   selector: 'app-metadata',
@@ -7,9 +10,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MetadataComponent implements OnInit {
 
-  constructor() { }
+  isLoading: boolean;
+  codeSnippet: string;
+
+  inlineCodeSnippets: SmallCodeSnippet[] = [];
+
+  private templatesErrorMessage: string;
+  private readonly templates_snippet_file: string = Constants.METADATA_FILE_PATH;
+
+  constructor(private _fileService: FileService) { }
 
   ngOnInit() {
-  }
+    this.inlineCodeSnippets.push({
+      inlineCode: '@Component({...metadata...})',
+      codeDescription: 'Decorates a class as Component'
+    });
+    this.inlineCodeSnippets.push({
+      inlineCode: '@NgModule({...metadata...})',
+      codeDescription: 'Decorates a class as Module'
+    });
+    this.inlineCodeSnippets.push({
+      inlineCode: '@Injectable()',
+      codeDescription: 'Decorates a class as Service'
+    });
+    this.inlineCodeSnippets.push({
+      inlineCode: '@Pipe',
+      codeDescription: 'Decorates a class as Pipe'
+    });
 
+    this._fileService.getFile(this.templates_snippet_file).subscribe(
+      response => {
+        this.codeSnippet = response;
+      },
+      error => {
+        this.templatesErrorMessage = error;
+      },
+      () => {
+        this.isLoading = true;
+      }
+    );
+  }
 }
--- src/app/shared-services/constants.ts
+++ src/app/shared-services/constants.ts
@@ -10,7 +10,7 @@ export class Constants {
     public static readonly TEMPLATES_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "templates.md";
     public static readonly TEMPLATES_HTML_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "templates.html";
 
-    public static readonly METADATA_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "metadata.md";
+    public static readonly METADATA_FILE_PATH: string = Constants.COMPONENTS_FILE_PATH;
 
     public static readonly DATA_BINDING_FILE_PATH: string = Constants.CODE_SNIPPETS_ROOT_DIR + "data-binding.md";
 
