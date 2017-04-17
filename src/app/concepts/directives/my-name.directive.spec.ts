1212a768352d9ac4aa11788dd3a3961398f1eefc
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-04-17 23:30:36 +0530

    myName custom attribute directive has been added.

---
+++ src/app/concepts/directives/my-name.directive.spec.ts
@@ -0,0 +1,8 @@
+import { MyNameDirective } from './my-name.directive';
+
+describe('MyNameDirective', () => {
+  it('should create an instance', () => {
+    const directive = new MyNameDirective();
+    expect(directive).toBeTruthy();
+  });
+});
