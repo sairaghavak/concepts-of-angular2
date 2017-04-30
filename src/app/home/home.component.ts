f9c918a42135fcc55c3fbba3ce526701e21b2e48
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-05-01 00:45:59 +0530

    Closing the modal popup on browser back button event when left unclosed.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,31 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, Renderer, ViewChild, AfterViewInit, OnDestroy } from '@angular/core';
+import { PlatformLocation } from '@angular/common';
 
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.css']
 })
-export class HomeComponent { 
-    isImageLoaded:boolean;
-    
+export class HomeComponent implements AfterViewInit {
+
+    @ViewChild("close_button") close_button: ElementRef;
+    isImageLoaded: boolean;
+
     onLoadAction() {
         this.isImageLoaded = true;
-    }   
+    }
+
+    constructor(private _renderer: Renderer, private _location: PlatformLocation) {
+
+    }
+
+    ngAfterViewInit() {
+        // Handling browsr back event: When user clicked on browser back button without closing the modal.
+        this._location.onPopState(
+            () => {
+                this._renderer.invokeElementMethod(this.close_button.nativeElement, "click");
+            }
+        );
+
+    }
 }
