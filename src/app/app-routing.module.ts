671dbc4aaf470a9ff74c18fcf7dd8220a9e3e36d
Sai Raghava Katepally <ksairaghava1@gmail.com>
2017-05-03 18:05:04 +0530

    Feedback Component added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,20 +3,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { PageNotFoundComponent } from './page-not-found.component';
-
+import { FeedbackComponent } from './feedback/feedback.component';
 
 const appRoutes: Routes = [
     /* Note that the sequence of routing is important*/
     { path: '', redirectTo: 'home', pathMatch: 'full' },
     { path: 'home', component: HomeComponent },
+    { path: 'feedback', component: FeedbackComponent },
     { path: 'concepts', loadChildren: './concepts/concepts.module#ConceptsModule' }, // I prefer this way of loading module
     // { path: 'home', loadChildren: () => ConceptsModule }, // This is other way of loading module rquires import at top of this file
     { path: '**', component: PageNotFoundComponent }
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(appRoutes, {useHash: true})],
+    imports: [RouterModule.forRoot(appRoutes, { useHash: true })],
     exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const appRoutingComponents = [HomeComponent, PageNotFoundComponent];
+export const appRoutingComponents = [HomeComponent, PageNotFoundComponent, FeedbackComponent];
